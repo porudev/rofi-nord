1f1e914526508f3140f5b5c74b15fc06894f609a
porudev <porudev@gmail.com>
2022-06-13 18:29:20 +0700

    added spotify

---
+++ spotify.sh
@@ -0,0 +1,34 @@
+
+rofi_command="rofi -theme ~/rofi-nord/.rasi/spotify.rasi"
+
+stop=""
+next=""
+previous=""
+play_pause=""
+
+# Variable passed to rofi
+options="$previous\n$play_pause\n$next\n$stop"
+
+# Get the current playing song
+current=$(playerctl --player=spotify metadata --format "{{ artist }} - {{ title }} - {{ album }}")
+# If mpd isn't running it will return an empty string, we don't want to display that
+if [[ -z "$current" ]]; then
+    current="No song is being played!"
+fi
+
+# Spawn the mpd menu with the "Play / Pause" entry selected by default
+chosen="$(echo -e "$options" | $rofi_command -p "  $current" -dmenu $active $urgent -selected-row 1)"
+case $chosen in
+    $previous)
+        playerctl --player=spotify previous
+        ;;
+    $play_pause)
+        playerctl --player=spotify play-pause
+        ;;
+    $next)
+        playerctl --player=spotify next
+        ;;
+    $stop)
+        killall spotify
+        ;;
+esac
